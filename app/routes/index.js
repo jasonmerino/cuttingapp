75d2b0c99c2a1b999f4f72daceae60b0cc40a14d
Adrian Miller <adrianjonmiller@gmail.com>
2015-02-13 18:57:02 -0800

    Commit

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	// res.render('index');
-  res.sendfile('./public/index.html');
+	res.render('index');
+  // res.sendfile('./public/index.html');
 });
 
 router.use(function(req, res, next) {
